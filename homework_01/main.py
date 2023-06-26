8fc5b27ad81d73f31d126b538579ad8b75c1a893
Maveliss <yarik.makarets@yandex.ru>
2023-06-26 11:50:07 +0300

    add homework

--- homework_01/main.py
+++ homework_01/main.py
@@ -16,16 +16,36 @@ def power_numbers(*numbers, power=2):
     <<< [1, 4, 25, 49]
     """
 
-print(power_numbers(1, 2, 5, 7))
+#print(power_numbers(1, 2, 5, 7))
+
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def filter_numbers(numbers_list, filter_type):
+    if filter_type == ODD:
+        return [number for number in numbers_list if number % 2 != 0]
+    if filter_type == EVEN:
+        return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        prime_numbers = []
+        for number in numbers_list:
+            if number < 2:
+                continue
+            for i in range(2, number):
+                if (number % i) == 0:
+                        break
+            else:
+                prime_numbers.append(number)
+        return prime_numbers
+
+
+#print(filter_numbers([1, 2, 3], ODD))
+#print(filter_numbers([2, 3, 4, 5], EVEN))
+print(filter_numbers([2, 3, 4, 5, 7, 11, 15, 17, 20, 25], PRIME))
 """
-def filter_numbers():
-    
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
@@ -34,4 +54,4 @@ def filter_numbers():
     <<< [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
-    """
+"""
